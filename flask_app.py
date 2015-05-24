ad6b8229c31bc54d1a0e71495d402aa9fe050e49
Hayley Denbraver <hayley.denbraver@gmail.com>
2015-05-24 00:16:24 -0700

    home page now displays aids cases diagnosed by year and sums the total number of aids cases through 2002. The total number matches the cdc number, which gives me confidence that i have written the query correctly.

--- flask_app.py
+++ flask_app.py
@@ -16,7 +16,40 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 @app.route("/")
 def single_page():
 	"""This renders the single page of the app."""
-	return render_template("home.html")
+
+	years = ['"1981"', 
+	'"1982"', 
+	'"1983"', 
+	'"1984"', 
+	'"1985"', 
+	'"1986"', 
+	'"1987"', 
+	'"1988"', 
+	'"1989"', 
+	'"1990"', 
+	'"1991"', 
+	'"1992"', 
+	'"1993"', 
+	'"1994"', 
+	'"1995"', 
+	'"1996"', 
+	'"1997"', 
+	'"1998"',
+	'"1999"', 
+	'"2000"', 
+	'"2001"', 
+	'"2002"' ]
+	case_load_dictionary = {}
+	total = 0
+	for year in years:
+		cases = model.sqla_session.query(model.Entry).filter_by(year_diagnosed_code=year).all()
+		case_count = 0
+		for case in cases:
+			case_count = case_count + case.cases
+		case_load_dictionary[year] = case_count
+		total = total + case_count
+
+	return render_template("home.html", case_load_dictionary = case_load_dictionary, keys = sorted(case_load_dictionary.keys()), total = total)
 
 
 if __name__ == "__main__":
