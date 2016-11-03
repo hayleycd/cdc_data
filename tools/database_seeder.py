987b6237be98219193e17f13ab4eb8911929a8b1
hayleycd <cook.hayley@gmail.com>
2016-11-02 20:20:39 -0700

    updated database seeder for new schema.

--- tools/database_seeder.py
+++ tools/database_seeder.py
@@ -13,16 +13,23 @@ def seed_from_file(myfile):
 			entry = model.RawEntry(year_diagnosed = attributes[0],
 				year_diagnosed_code = attributes[1], 
 				location = attributes[2],
-				location_code = attributes[3], 
-				age_at_diagnosis = attributes[4], 
-				age_at_diagnosis_code = attributes[5],
-				exposure_category = attributes[6],
-				exposure_code = attributes[7],
-				sex_and_orientation = attributes[8],
-				sex_and_orientation_code = attributes[9],
+				location_code = attributes[3],
 				cases = int(attributes[10])
 				)
 
 			model.sqla_session.add(entry)
 
 	model.sqla_session.commit()
+
+def seed_initial_db():
+	files_to_load = ["1986_1989.txt",
+					 "1990_1993.txt",
+					 "1994_1996.txt",
+					 "1997_1999.txt",
+					 "2000_2002.txt",
+					 "pre_1985.txt",]
+
+	for file in files_to_load:
+		current_file = "../data_from_wonder/" + file
+		seed_from_file(current_file)
+
